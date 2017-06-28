0d655fc873eb210f0be3994ab0d0b15149c267d8
vydd <vidovicd@gmail.com>
2017-06-28 18:14:43 +0200

    Breaking changes in paho minor release

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name='allthingstalk',
-    version='0.1.5',
+    version='0.1.6',
     description='AllThingsTalk Python SDK',
     url='https://github.com/allthingstalk/python-sdk',
-    download_url='https://github.com/allthingstalk/python-sdk/archive/0.1.5.tar.gz',
+    download_url='https://github.com/allthingstalk/python-sdk/archive/0.1.6.tar.gz',
     author='Danilo Vidovic',
     author_email='dv@allthingstalk.com',
     license='Apache 2.0',
     packages=['allthingstalk'],
     keywords=['allthingstalk', 'iot', 'sdk'],
     install_requires=[
-        'paho-mqtt',
+        'paho-mqtt==1.2.3',
         'requests',
         'python-dateutil',
         'six'
