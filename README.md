892aa664a498e30200ae3072d3407a6c271fd7ae
vydd <vidovicd@gmail.com>
2017-06-28 11:31:13 +0200

    Version bump, production readme

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ AllThingsTalk Python SDK provides Python APIs to manage and implement AllThingsT
 
 ## Installation
 
-Install using `pip` (still on testing):
+```
+pip install allthingstalk
+```
 
-```
-pip install --extra-index-url https://testpypi.python.org/pypi allthingstalk
-```
+## Documentation
+
+http://docs.allthingstalk.com/developers/libraries/python/
 
 # License
 
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='allthingstalk',
-    version='0.1.4',
+    version='0.1.5',
     description='AllThingsTalk Python SDK',
     url='https://github.com/allthingstalk/python-sdk',
-    download_url='https://github.com/allthingstalk/python-sdk/archive/0.1.3.tar.gz',
+    download_url='https://github.com/allthingstalk/python-sdk/archive/0.1.5.tar.gz',
     author='Danilo Vidovic',
     author_email='dv@allthingstalk.com',
     license='Apache 2.0',
