e5b43c3a942dd77c8f2d48a542aaf8dd98f26be5
vydd <vidovicd@gmail.com>
2017-08-31 11:39:26 +0200

    Updates version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='allthingstalk',
-    version='0.2.3',
+    version='0.2.4',
     description='AllThingsTalk Python SDK',
     url='https://github.com/allthingstalk/python-sdk',
-    download_url='https://github.com/allthingstalk/python-sdk/archive/0.2.3.tar.gz',
+    download_url='https://github.com/allthingstalk/python-sdk/archive/0.2.4.tar.gz',
     author='Danilo Vidovic',
     author_email='dv@allthingstalk.com',
     license='Apache 2.0',
